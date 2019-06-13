6bf888fda348b06c2a4ae24f30246314df2cfa1a
Shuotian Cheng <shuche@microsoft.com>
2019-06-13 16:09:06 -0700

    [jenkins]: Move script out of Jenkinsfile for re-use purpose (#35)
    Make the files DRY
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

---
+++ scripts/common/sonic-swss-common-build/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+
+# Install SWIG
+sudo dpkg -i buildimage/target/debs/stretch/swig3.0_*.deb
+
+# Install Redis
+sudo dpkg -i buildimage/target/debs/stretch/redis-tools_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/redis-server_*.deb
+
+# Install HIREDIS
+sudo dpkg -i buildimage/target/debs/stretch/libhiredis*.deb
+
+# Install libnl3
+sudo dpkg -i buildimage/target/debs/stretch/libnl-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/libnl-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/libnl-genl-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/libnl-genl-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/libnl-route-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/libnl-route-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/libnl-nf-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/libnl-nf-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/libnl-cli-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/libnl-cli-3-dev_*.deb
+
+pushd sonic-swss-common
+
+./autogen.sh
+fakeroot debian/rules binary
+
+popd
+
+mkdir target
+cp *.deb target/
+
