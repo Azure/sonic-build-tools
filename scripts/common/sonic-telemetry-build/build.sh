3d0f405f3d678431d6c2754f95280fc1cd79be5d
Shu0T1an ChenG <shuche@microsoft.com>
2019-07-19 14:43:41 -0700

    [jenkins]: Add sonic-telemetry build script
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

---
+++ scripts/common/sonic-telemetry-build/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+# Install HIREDIS
+sudo dpkg -i buildimage/target/debs/stretch/libhiredis*.deb
+
+# Install REDIS
+sudo dpkg -i buildimage/target/debs/stretch/redis-tools_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/redis-server_*.deb
+sudo sed -ri 's/^# unixsocket/unixsocket/' /etc/redis/redis.conf
+sudo sed -ri 's/^unixsocketperm .../unixsocketperm 777/' /etc/redis/redis.conf
+sudo sed -ri 's/redis-server.sock/redis.sock/' /etc/redis/redis.conf
+sudo service redis-server start
+
+pushd sonic-telemetry
+
+dpkg-buildpackage -rfakeroot -b -us -uc
+
+popd
+
+mkdir target
+cp *.deb target/
+
