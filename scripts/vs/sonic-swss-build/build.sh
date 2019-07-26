b6dbd0a2871060288cb2bedda599742b8d88ffb4
Shu0T1an ChenG <shuche@microsoft.com>
2019-07-26 10:46:33 -0700

    add credential

--- scripts/vs/sonic-swss-build/build.sh
+++ scripts/vs/sonic-swss-build/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -ex
 
+docker login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWD sonicdev-microsoft.azurecr.io:443
 docker run --rm=true --privileged -v $(pwd):/sonic -w /sonic -i sonic-slave-stretch-johnar ./scripts/vs/sonic-swss-build/build_in_docker.sh
