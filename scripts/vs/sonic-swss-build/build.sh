75189a721cff127e3d5871093be97fd5c0d76e3a
Shu0T1an ChenG <shuche@microsoft.com>
2019-07-26 10:53:22 -0700

    add pull
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- scripts/vs/sonic-swss-build/build.sh
+++ scripts/vs/sonic-swss-build/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 
 docker login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWD sonicdev-microsoft.azurecr.io:443
-docker run --rm=true --privileged -v $(pwd):/sonic -w /sonic -i sonic-slave-stretch-johnar ./scripts/vs/sonic-swss-build/build_in_docker.sh
+docker pull sonicdev-microsoft.azurecr.io:443/sonic-slave-stretch:latest
+docker run --rm=true --privileged -v $(pwd):/sonic -w /sonic -i sonicdev-microsoft.azurecr.io:443/sonic-slave-stretch:latest ./scripts/vs/sonic-swss-build/build_in_docker.sh
