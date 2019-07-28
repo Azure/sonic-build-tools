94648ab57cddb648f182d087b529e1309c7465e9
Guohan Lu <gulv@microsoft.com>
2019-07-28 21:13:23 +0000

    [jenkins]: build docker-sonic-vs with specific tags

--- scripts/vs/sonic-swss-build/build.sh
+++ scripts/vs/sonic-swss-build/build.sh
@@ -1,5 +1,24 @@
 #!/bin/bash -ex
 
+echo ${JOB_NAME##*/}.${BUILD_NUMBER}
+
 docker login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWD sonicdev-microsoft.azurecr.io:443
 docker pull sonicdev-microsoft.azurecr.io:443/sonic-slave-stretch:latest
 docker run --rm=true --privileged -v $(pwd):/sonic -w /sonic -i sonicdev-microsoft.azurecr.io:443/sonic-slave-stretch:latest ./scripts/vs/sonic-swss-build/build_in_docker.sh
+
+mkdir -p scripts/vs/sonic-swss-build/debs
+cp *.deb scripts/vs/sonic-swss-build/debs
+cp sairedis/*.deb scripts/vs/sonic-swss-build/debs
+cp common/*.deb scripts/vs/sonic-swss-build/debs
+cp utilities/*.deb scripts/vs/sonic-swss-build/debs
+
+docker load < buildimage/target/docker-sonic-vs.gz
+
+pushd scripts/vs/sonic-swss-build
+mkdir -p docker-sonic-vs/debs
+sudo mount --bind debs docker-sonic-vs/debs
+docker build --squash --no-cache -t docker-sonic-vs:${JOB_NAME##*/}.${BUILD_NUMBER} docker-sonic-vs
+sudo umount docker-sonic-vs/debs
+popd
+
+docker save docker-sonic-vs:${JOB_NAME##*/}.${BUILD_NUMBER} | gzip -c > buildimage/target/docker-sonic-vs.gz
--- scripts/vs/sonic-swss-build/test.sh
+++ scripts/vs/sonic-swss-build/test.sh
@@ -4,24 +4,7 @@ cleanup() {
     sudo ip -all netns delete
 }
 
-mkdir -p scripts/vs/sonic-swss-build/debs
-cp *.deb scripts/vs/sonic-swss-build/debs
-cp sairedis/*.deb scripts/vs/sonic-swss-build/debs
-cp common/*.deb scripts/vs/sonic-swss-build/debs
-cp utilities/*.deb scripts/vs/sonic-swss-build/debs
-
-docker load < buildimage/target/docker-sonic-vs.gz
-
-pushd scripts/vs/sonic-swss-build
-mkdir -p docker-sonic-vs/debs
-sudo mount --bind debs docker-sonic-vs/debs
-docker build --squash --no-cache -t docker-sonic-vs docker-sonic-vs
-sudo umount docker-sonic-vs/debs
-popd
-
-docker save docker-sonic-vs | gzip -c > buildimage/target/docker-sonic-vs.gz
-
 trap cleanup ERR
 
 pushd swss/tests
-sudo py.test -v --junitxml=tr.xml
+sudo py.test -v --junitxml=tr.xml --imgname=docker-sonic-vs:${JOB_NAME##*/}.${BUILD_NUMBER}
