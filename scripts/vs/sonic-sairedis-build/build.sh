c49e820f8e23da9d0e372ead306eee9c855813b8
Guohan Lu <gulv@microsoft.com>
2019-09-18 18:30:16 +0000

    [jenkins]: archive syslog for sonic sairedis

--- scripts/vs/sonic-sairedis-build/build.sh
+++ scripts/vs/sonic-sairedis-build/build.sh
@@ -26,6 +26,15 @@ sudo sed -ri 's/^# unixsocket/unixsocket/' /etc/redis/redis.conf
 sudo sed -ri 's/^unixsocketperm .../unixsocketperm 777/' /etc/redis/redis.conf
 sudo sed -ri 's/redis-server.sock/redis.sock/' /etc/redis/redis.conf
 sudo service redis-server start
+sudo service rsyslog start
+
+cleanup() {
+    mkdir -p ../target
+    sudo cp /var/log/syslog ../target/
+    sudo chmod 644 ../target/syslog
+}
+
+trap cleanup ERR
 
 pushd sairedis
 
@@ -36,3 +45,5 @@ popd
 
 mkdir -p target
 cp *.deb target/
+sudo cp /var/log/syslog target/
+sudo chmod 644 target/syslog
