2355866e7da4c6be64379ba31046d2ce7f0dc813
Guohan Lu <gulv@microsoft.com>
2019-11-19 08:17:31 +0000

    [jenkins]: installed required packages for redis-tools in sonic-sairedis-build
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- scripts/vs/sonic-sairedis-build/build.sh
+++ scripts/vs/sonic-sairedis-build/build.sh
@@ -20,12 +20,16 @@ sudo dpkg -i common/libswsscommon_*.deb
 sudo dpkg -i common/libswsscommon-dev_*.deb
 
 # Install REDIS
+sudo apt-get install -y liblua5.1-0 lua-bitop lua-cjson
 sudo dpkg -i buildimage/target/debs/stretch/redis-tools_*.deb
 sudo dpkg -i buildimage/target/debs/stretch/redis-server_*.deb
 sudo sed -ri 's/^# unixsocket/unixsocket/' /etc/redis/redis.conf
 sudo sed -ri 's/^unixsocketperm .../unixsocketperm 777/' /etc/redis/redis.conf
 sudo sed -ri 's/redis-server.sock/redis.sock/' /etc/redis/redis.conf
 sudo service redis-server start
+
+# Start rsyslog
+sudo apt-get install -y rsyslog
 sudo service rsyslog start
 
 cleanup() {
