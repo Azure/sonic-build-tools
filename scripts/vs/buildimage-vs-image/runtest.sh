bc7d2ec284d6cda1b5ee9d9d5461d8f32a7cf487
Guohan Lu <gulv@microsoft.com>
2019-11-22 06:44:45 +0000

    increase sleep to 180 seconds for vs image test
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- scripts/vs/buildimage-vs-image/runtest.sh
+++ scripts/vs/buildimage-vs-image/runtest.sh
@@ -8,7 +8,7 @@ cd /data/sonic-mgmt/ansible
 ./testbed-cli.sh -m veos.vtb -t vtestbed.csv refresh-dut vms-kvm-t0 lab password.txt || true
 sleep 3
 ./testbed-cli.sh -m veos.vtb -t vtestbed.csv deploy-mg vms-kvm-t0 lab password.txt
-sleep 30
+sleep 180
 export ANSIBLE_LIBRARY=/data/sonic-mgmt/ansible/library/
 cd /data/sonic-mgmt/tests
 py.test --inventory veos.vtb --host-pattern all --user admin -vvv --show-capture stdout --testbed vms-kvm-t0 --testbed_file vtestbed.csv test_bgp_fact.py
