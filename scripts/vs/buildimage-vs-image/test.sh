25ccaa77e5a7a88ec797523b70c98e6ca3675871
Guohan Lu <gulv@microsoft.com>
2019-11-23 18:15:01 +0000

    [jenkins]: pull latest docker-sonic-mgmt for kvmtest
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- scripts/vs/buildimage-vs-image/test.sh
+++ scripts/vs/buildimage-vs-image/test.sh
@@ -4,6 +4,9 @@ echo ${JOB_NAME##*/}.${BUILD_NUMBER}
 
 ls -l
 
+docker login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWD sonicdev-microsoft.azurecr.io:443
+docker pull sonicdev-microsoft.azurecr.io:443/docker-sonic-mgmt:latest
+
 cat $PRIVATE_KEY > pkey.txt
 
 mkdir -p $HOME/sonic-vm/images
@@ -16,4 +19,4 @@ cd sonic-mgmt/ansible
 sed -i s:use_own_value:johnar: veos.vtb
 echo abc > password.txt
 cd ../../
-docker run --rm=true -v $(pwd):/data -w /data -i docker-sonic-mgmt ./scripts/vs/buildimage-vs-image/runtest.sh
+docker run --rm=true -v $(pwd):/data -w /data -i sonicdev-microsoft.azurecr.io:443/docker-sonic-mgmt ./scripts/vs/buildimage-vs-image/runtest.sh
