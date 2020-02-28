8e1c9f47d091f2d7407900fd6d864c1f17f39eb5
Rupesh Kumar <rupesh-k.kumar@broadcom.com>
2020-02-28 07:30:32 -0800

    [jenkins]: Add sonic-mgmt-framework build job

---
+++ scripts/common/sonic-mgmt-framework-build/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+
+# Install HIREDIS
+sudo dpkg -i buildimage/target/debs/stretch/libhiredis*.deb
+
+# Install REDIS
+sudo apt-get install -y liblua5.1-0 lua-bitop lua-cjson
+sudo dpkg -i buildimage/target/debs/stretch/redis-tools_*.deb
+sudo dpkg -i buildimage/target/debs/stretch/redis-server_*.deb
+sudo sed -ri 's/^# unixsocket/unixsocket/' /etc/redis/redis.conf
+sudo sed -ri 's/^unixsocketperm .../unixsocketperm 777/' /etc/redis/redis.conf
+sudo sed -ri 's/redis-server.sock/redis.sock/' /etc/redis/redis.conf
+sudo service redis-server start
+
+#Install libyang
+sudo dpkg -i buildimage/target/debs/stretch/libyang*.deb
+
+pushd sonic-mgmt-framework
+
+dpkg-buildpackage -rfakeroot -b -us -uc
+
+popd
+
+mkdir -p target
+cp *.deb target/
+
