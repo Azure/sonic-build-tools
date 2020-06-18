4ab8ea90b228310caede544ccf822a32e93cf55f
Guohan Lu <lguohan@gmail.com>
2020-06-18 10:39:02 +0000

    [kvmtest]: save sonic vm state for post analysis when test fails

--- scripts/vs/buildimage-vs-image/test.sh
+++ scripts/vs/buildimage-vs-image/test.sh
@@ -4,6 +4,9 @@ echo ${JOB_NAME##*/}.${BUILD_NUMBER}
 
 ls -l
 
+tbname=vms-kvm-t0
+dut=vlab-01
+
 docker login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWD sonicdev-microsoft.azurecr.io:443
 docker pull sonicdev-microsoft.azurecr.io:443/docker-sonic-mgmt:latest
 
@@ -23,4 +26,18 @@ cd sonic-mgmt/ansible
 sed -i s:use_own_value:johnar: veos.vtb
 echo abc > password.txt
 cd ../../
-docker run --rm=true -v $(pwd):/data -w /data -i sonicdev-microsoft.azurecr.io:443/docker-sonic-mgmt ./scripts/vs/buildimage-vs-image/runtest.sh
+docker run --rm=true -v $(pwd):/data -w /data -i sonicdev-microsoft.azurecr.io:443/docker-sonic-mgmt ./scripts/vs/buildimage-vs-image/runtest.sh $tbname
+
+# save dut state if test fails
+if [ $? != 0 ]; then
+    virsh_version=$(virsh --version)
+    if [ $virsh_version == "6.0.0" ]; then
+        rm -rf kvmdump
+        mkdir -p kvmdump
+        virsh save $dut kvmdump/$dut.memdmp
+        virsh dumpxml $dut > kvmdump/$dut.xml
+        img=$(virsh domblklist $dut | grep vda | awk '{print $2}')
+        cp $img kvmdump/$dut.img
+        virsh undefine $dut
+    fi
+fi
