c267563081eb16fdcffe4f8276c3d4bdb5d53dd1
Xin Wang <wangxin.wang@gmail.com>
2020-06-19 10:57:42 +0800

    [mgmt] Fix sonic-mgmt pr test no result issue (#117)
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>

--- scripts/mgmt/test.sh
+++ scripts/mgmt/test.sh
@@ -2,6 +2,8 @@
 
 echo ${JOB_NAME##*/}.${BUILD_NUMBER}
 
+tbname=vms-kvm-t0
+
 docker login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWD sonicdev-microsoft.azurecr.io:443
 docker pull sonicdev-microsoft.azurecr.io:443/docker-sonic-mgmt:latest
 
@@ -20,4 +22,4 @@ sed -i s:use_own_value:johnar: veos.vtb
 echo abc > password.txt
 popd
 
-docker run --rm=true -v $(pwd):/data -w /data -i sonicdev-microsoft.azurecr.io:443/docker-sonic-mgmt ./scripts/vs/buildimage-vs-image/runtest.sh
+docker run --rm=true -v $(pwd):/data -w /data -i sonicdev-microsoft.azurecr.io:443/docker-sonic-mgmt ./scripts/vs/buildimage-vs-image/runtest.sh $tbname
