2b0436ba65e5bd78dc28d4b787405a9c65791f17
Praveen Chaudhary <pchaudhary@linkedin.com>
2020-06-29 11:22:06 -0700

    [sonic-utilities-build/build.sh]: Add sonic_yang_models as dep to sonic utils. (#127)
    Since we can not refer a dir in sonic-buildimage while jenkins testing of sonic-utilities.
    We need to create build dependency on sonic_yang_models PKG too.
    Related to that, we need this change in build.sh script.
    
    Signed-off-by: Praveen Chaudhary pchaudhary@linkedin.com

--- scripts/common/sonic-utilities-build/build.sh
+++ scripts/common/sonic-utilities-build/build.sh
@@ -21,6 +21,8 @@ sudo pip install pytest-runner==4.4
 sudo pip install xmltodict==0.12.0
 sudo pip install jsondiff==1.2.0
 
+sudo pip3 install buildimage/target/python-wheels/sonic_yang_models-1.0-py3-none-any.whl
+
 sudo dpkg -i buildimage/target/debs/stretch/libyang_1.0.73_amd64.deb
 sudo dpkg -i buildimage/target/debs/stretch/libyang-cpp_1.0.73_amd64.deb
 sudo dpkg -i buildimage/target/debs/stretch/python2-yang_1.0.73_amd64.deb
