b2646757bb3313f4c079228d3de2b37670d07f22
Sachin Holla <sachin.holla@broadcom.com>
2020-06-30 09:38:50 +0530

    Fix sonic-telemetry build issues
    1) Cleaned up sonic-trelemetry build script to remove all
    sonic-mgmt-common related steps. Jenkinsfile now includes steps to clone
    and build sonic-mgmt-common diretly. This solves the redis installation
    errors seens in telemetry PR builds.
    
    2) Avoid compilation of sonic-mgmt-common's go test files during
    sonic-mgmt-famework build. Quickfix for test code issues in mgmt-common.

--- scripts/common/sonic-telemetry-build/build.sh
+++ scripts/common/sonic-telemetry-build/build.sh
@@ -1,30 +1,10 @@
 #!/bin/bash -ex
 
+# Build script for sonic-telemetry.
+# Assumes sonic-mgmt-common is already cloned & built.
+
 # Install HIREDIS
 sudo dpkg -i buildimage/target/debs/stretch/libhiredis*.deb
-
-# Install REDIS
-sudo apt-get install -y liblua5.1-0 lua-bitop lua-cjson
-sudo dpkg -i buildimage/target/debs/stretch/redis-tools_*.deb
-sudo dpkg -i buildimage/target/debs/stretch/redis-server_*.deb
-sudo sed -ri 's/^# unixsocket/unixsocket/' /etc/redis/redis.conf
-sudo sed -ri 's/^unixsocketperm .../unixsocketperm 777/' /etc/redis/redis.conf
-sudo sed -ri 's/redis-server.sock/redis.sock/' /etc/redis/redis.conf
-sudo service redis-server start
-
-# Install libyang
-sudo dpkg -i buildimage/target/debs/stretch/libyang*.deb
-
-# Clone sonic-mgmt-common repository
-git clone https://github.com/Azure/sonic-mgmt-common.git
-
-# Build sonic-mgmt-common first
-
-pushd sonic-mgmt-common
-
-dpkg-buildpackage -rfakeroot -b -us -uc
-
-popd
 
 # Build sonic-telemetry
 
@@ -36,4 +16,3 @@ popd
 
 mkdir -p target
 cp *.deb target/
-
