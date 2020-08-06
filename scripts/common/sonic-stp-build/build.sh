3da4122c8305a422f34135bda2a9d3337cbb3966
Guohan Lu <lguohan@gmail.com>
2020-08-06 01:20:28 -0700

    [stp]: update stp build to buster

--- scripts/common/sonic-stp-build/build.sh
+++ scripts/common/sonic-stp-build/build.sh
@@ -1,15 +1,24 @@
 #!/bin/bash -xe
 
-#install libevent_2
-sudo apt-get -y purge libevent-dev
-sudo dpkg -i target/debs/stretch/libevent_2.*.deb
 
 #install HIREDIS
-sudo dpkg -i target/debs/stretch/libhiredis*.deb
+sudo apt-get install -y libhiredis0.14 libhiredis-dev
+
+# Install libnl3
+sudo dpkg -i buildimage/target/debs/buster/libnl-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-genl-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-genl-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-route-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-route-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-nf-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-nf-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-cli-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-cli-3-dev_*.deb
 
 #install libswsscommon
-sudo dpkg -i target/debs/stretch/libswsscommon_*.deb
-sudo dpkg -i target/debs/stretch/libswsscommon-dev_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libswsscommon_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libswsscommon-dev_*.deb
 
 pushd sonic-stp
 
