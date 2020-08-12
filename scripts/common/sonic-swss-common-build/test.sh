f911edb81360e3bd237a1253e2df7472184cccbe
Danny Allen <daall@microsoft.com>
2020-08-11 18:42:03 -0700

    Remove uneccesary move to target directory

--- scripts/common/sonic-swss-common-build/test.sh
+++ scripts/common/sonic-swss-common-build/test.sh
@@ -2,9 +2,5 @@
 
 echo ${JOB_NAME##*/}.${BUILD_NUMBER}
 
-pushd target
-
 docker pull sonicdev-microsoft.azurecr.io:443/sonic-slave-buster-johnar:latest
 docker run --rm=true --privileged -v $(pwd):/sonic -w /sonic -i sonicdev-microsoft.azurecr.io:443/sonic-slave-buster-johnar:latest ./scripts/common/sonic-swss-common-build/docker_test_script.sh
-
-popd
