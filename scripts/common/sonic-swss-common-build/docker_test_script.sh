a608fc9cc07f9a7bc4051f032711e187843189cf
Danny Allen <daall@microsoft.com>
2020-08-13 13:12:51 -0700

    Add libnl for the unit testing

--- scripts/common/sonic-swss-common-build/docker_test_script.sh
+++ scripts/common/sonic-swss-common-build/docker_test_script.sh
@@ -9,6 +9,18 @@ sudo sed -ri 's/^unixsocketperm .../unixsocketperm 777/' /etc/redis/redis.conf
 sudo sed -ri 's/redis-server.sock/redis.sock/' /etc/redis/redis.conf
 sudo service redis-server restart
 
+# Install libnl3
+sudo dpkg -i buildimage/target/debs/buster/libnl-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-genl-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-genl-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-route-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-route-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-nf-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-nf-3-dev_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-cli-3-200_*.deb
+sudo dpkg -i buildimage/target/debs/buster/libnl-cli-3-dev_*.deb
+
 sudo dpkg -i libswsscommon_*.deb
 sudo dpkg -i python-swsscommon_*.deb
 
