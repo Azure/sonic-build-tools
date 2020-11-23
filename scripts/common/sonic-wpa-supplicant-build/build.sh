c4f72c2b2a895a36ba8b7073242aed71b40af76f
Ze Gan <ganze718@gmail.com>
2020-11-23 16:50:10 +0800

    Remove sonic-mgmt-common
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- scripts/common/sonic-wpa-supplicant-build/build.sh
+++ scripts/common/sonic-wpa-supplicant-build/build.sh
@@ -1,26 +1,5 @@
 #!/bin/bash -ex
 
-# Install HIREDIS
-sudo apt-get install -y libhiredis0.14 libhiredis-dev
-
-# Install REDIS
-sudo apt-get install -y redis-server
-sudo sed -ri 's/^# unixsocket/unixsocket/' /etc/redis/redis.conf
-sudo sed -ri 's/^unixsocketperm .../unixsocketperm 777/' /etc/redis/redis.conf
-sudo sed -ri 's/redis-server.sock/redis.sock/' /etc/redis/redis.conf
-sudo service redis-server start
-
-# Install libyang
-sudo dpkg -i buildimage/target/debs/buster/libyang*.deb
-
-# Build sonic-mgmt-common first
-
-pushd sonic-mgmt-common
-
-NO_TEST_BINS=1 dpkg-buildpackage -rfakeroot -b -us -uc
-
-popd
-
 # Build sonic-wpa-supplicant
 
 pushd sonic-wpa-supplicant
@@ -31,4 +10,3 @@ popd
 
 mkdir -p target
 cp *.deb target/
-
