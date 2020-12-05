e7b159c4b1d2393b42c762bd3d9ef346462d37fe
Guohan Lu <lguohan@gmail.com>
2020-12-04 18:53:13 -0800

    [sonic-slave]: push sonic-slave base image to registry
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- scripts/bldenv/sonic-slave/build.sh
+++ scripts/bldenv/sonic-slave/build.sh
@@ -10,9 +10,11 @@ USER=`id -un`
 CONFIGURED_ARCH=amd64
 SLAVE_DIR=sonic-slave-$DISTRO
 CONFIGURED_ARCH=${CONFIGURED_ARCH} j2 ${SLAVE_DIR}/Dockerfile.j2 > ${SLAVE_DIR}/Dockerfile
+SLAVE_BASE_TAG=$(sha1sum ${SLAVE_DIR}/Dockerfile | awk '{print substr($1,0,11);}')
 SLAVE_TAG=$(cat ${SLAVE_DIR}/Dockerfile.user ${SLAVE_DIR}/Dockerfile | sha1sum | awk '{print substr($1,0,11);}')
 
 echo $USER
+echo $SLAVE_BASE_TAG
 echo $SLAVE_TAG
 
 BLDENV=$DISTRO make sonic-slave-build
@@ -28,6 +30,8 @@ REGISTRY_SERVER=sonicdev-microsoft.azurecr.io
 
 docker tag sonic-slave-$DISTRO-$USER:$SLAVE_TAG local/sonic-slave-$DISTRO-$USER:latest
 docker tag sonic-slave-$DISTRO-$USER:$SLAVE_TAG $REGISTRY_SERVER:$REGISTRY_PORT/sonic-slave-$DISTRO-$USER:latest
+docker tag sonic-slave-$DISTRO:$SLAVE_BASE_TAG $REGISTRY_SERVER:$REGISTRY_PORT/sonic-slave-$DISTRO:latest
 
 docker login -u $REGISTRY_USERNAME -p "$REGISTRY_PASSWD" $REGISTRY_SERVER:$REGISTRY_PORT
+docker push $REGISTRY_SERVER:$REGISTRY_PORT/sonic-slave-$DISTRO:latest
 docker push $REGISTRY_SERVER:$REGISTRY_PORT/sonic-slave-$DISTRO-$USER:latest
