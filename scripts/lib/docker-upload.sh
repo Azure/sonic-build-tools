25890685c8e096afd66e2900e1f20fb0c22535b2
Guohan Lu <lguohan@gmail.com>
2020-12-31 22:15:55 -0800

    [docker-upload]: stop push build version tag to the repository
    only push latest iamge to the repository
    
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- scripts/lib/docker-upload.sh
+++ scripts/lib/docker-upload.sh
@@ -15,9 +15,9 @@ docker login -u $REGISTRY_USERNAME -p "$REGISTRY_PASSWD" $REGISTRY_SERVER:$REGIS
 ## Prepare tag
 docker_image_name=$(basename $DOCKER_IMAGE_FILE | cut -d. -f1)
 remote_image_name=$REGISTRY_SERVER:$REGISTRY_PORT/$docker_image_name:$DOCKER_IMAGE_TAG
-timestamp="$(date -u +%Y%m%d)"
-build_version="${timestamp}.bld-$BUILD_NUMBER"
-build_remote_image_name=$REGISTRY_SERVER:$REGISTRY_PORT/$docker_image_name:$build_version
+# timestamp="$(date -u +%Y%m%d)"
+# build_version="${timestamp}.bld-$BUILD_NUMBER"
+# build_remote_image_name=$REGISTRY_SERVER:$REGISTRY_PORT/$docker_image_name:$build_version
 
 ## Load docker image
 docker load -i $DOCKER_IMAGE_FILE_DIR/$DOCKER_IMAGE_FILE
@@ -25,7 +25,7 @@ docker load -i $DOCKER_IMAGE_FILE_DIR/$DOCKER_IMAGE_FILE
 ## Add registry information as tag, so will push as latest
 ## Add additional tag with build information
 docker tag $docker_image_name $remote_image_name
-docker tag $docker_image_name $build_remote_image_name
+# docker tag $docker_image_name $build_remote_image_name
 
 ## Push image to registry server
 ## And get the image digest SHA256
@@ -34,7 +34,7 @@ image_sha=$(docker push $remote_image_name | sed -n "s/.*: digest: sha256:\([0-9
 docker rmi $remote_image_name || true
 echo "Image sha256: $image_sha"
 
-echo "Pushing $build_remote_image_name"
-docker push $build_remote_image_name
-docker rmi $build_remote_image_name || true
+# echo "Pushing $build_remote_image_name"
+# docker push $build_remote_image_name
+# docker rmi $build_remote_image_name || true
 docker rmi $docker_image_name || true
